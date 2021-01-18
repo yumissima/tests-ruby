e4483366dc563b342f611849c89f3b6569d3c785
yumissima <pascalelaunois@gmail.com>
2021-01-18 22:51:00 +0100

    later

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,15 @@
+def simon_say(echo)
+    return " #{echo}"
+end
+
+def shout(word)
+    return word.upcase
+end
+
+def multiple(word)
+    return word.upcase 
+end
+
+def repeat(word)
+    return "#{word} #{word}"
+end   
